a1663999d10696c24f9cec0a5cca02af2b5db85e
Vahin Sharma <vahinsharma@gmail.com@users.noreply.github.com>
2020-08-17 18:09:40 +0530

    ­

--- index.py
+++ index.py
@@ -1,6 +1,4 @@
 from pandas import read_csv as rc
 from plotly.express import scatter
 
-df = rc("pixelMath.csv")
-fig = scatter(df, x='student_id', y='level', color="attempt", size='size')
-fig.show()
+scatter(rc("pixelMath.csv"), x='student_id', y='level', color="attempt", size='size').show()
